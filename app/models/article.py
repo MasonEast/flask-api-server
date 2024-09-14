354a43529654414eb125760cabda3c8d07805e64
mason <2224742726@qq.com>
2024-09-14 13:41:55 +0800

    文章和评论的modal

--- app/models/article.py
+++ app/models/article.py
@@ -8,18 +8,63 @@ from utils import HashBcrypt
 class Article(db.Model):
     article_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-
-    phone = db.Column(db.String(20), nullable=True)
-    avatar_url = db.Column(db.String(255), nullable=True)
-    intro = db.Column(db.Text, nullable=True) # 简介
-    birthday = db.Column(db.Date, nullable=True)
-    gender = db.Column(db.String(10), nullable=True) # 性别
-
-    status = db.Column(db.Integer, nullable=False) # 状态 1 在线， 0 离线
+    author = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text, nullable=False) # 内容
+    tags = db.Column(db.String(255), nullable=False)
     create_time = db.Column(db.String(100), nullable=False)
-    last_login_time = db.Column(db.String(100), nullable=False)
+
+    
+    views = db.Column(db.Integer, nullable=False) # 浏览量
+    likes = db.Column(db.Integer, nullable=False) # 点赞量
+    collects = db.Column(db.Integer, nullable=False) # 收藏量
+    transmits = db.Column(db.Integer, nullable=False) # 转发量
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def rollback(self):
+        db.session.rollback()
+
+    def check_password(self, password):
+        return HashBcrypt.check(password, self.password)
+    
+    def set_status(self, status):
+        self.status = status
+
+    def toJSON(self):
+
+        cls_dict = {}
+        cls_dict['_id'] = self.article_id
+        cls_dict['phone'] = self.phone
+        cls_dict['email'] = self.email
+
+        return cls_dict
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.get_or_404(id)
+
+    @classmethod
+    def get_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+    
+    @classmethod
+    def get_by_phone(cls, phone):
+        return cls.query.filter_by(phone=phone).first()
+
+class Comment(db.Model):
+    comment_id = db.Column(db.Integer, primary_key=True)
+    article_id = db.Column(db.Integer) # 关联文章
+    author = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text, nullable=False) # 内容
+    create_time = db.Column(db.String(100), nullable=False)
+
+    
+    views = db.Column(db.Integer, nullable=True) # 浏览量
+    likes = db.Column(db.Integer, nullable=True) # 点赞量
+    collects = db.Column(db.Integer, nullable=True) # 收藏量
+    transmits = db.Column(db.Integer, nullable=True) # 转发量
 
     def save(self):
         db.session.add(self)
